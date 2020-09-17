d9de43b3f6ca85f409861d5476f195d99d731ef9
ANKAY <52236029+ankay212000@users.noreply.github.com>
2020-09-17 15:53:28 +0530

    Update Find subarray with given sum.cpp

--- Arrays/Find subarray with given sum.cpp
+++ Arrays/Find subarray with given sum.cpp
@@ -1,10 +1,10 @@
 #include<bits/stdc++.h>
 #define ll long long
-#define io ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
 using namespace std;
 #define N 100000
-#define mod 1000000007
- 
+#define limit 10000
+
 template <typename T>
 void print(T x){cout<<x<<"\n";}
 template <typename T1, typename T2>
@@ -12,46 +12,36 @@ void print2(T1 x,T2 y){cout<<x<<" "<<y<<"\n";}
 template <typename T1, typename T2,typename T3>
 void print3(T1 x, T2 y,T3 z){cout<<x<<" "<<y<<" "<<z<<"\n";}
 
-void subarray(ll arr[],ll n,int k)
-{
-    ll sum=0;
-    map<int,int> num;
-    for(int i=0;i<n;i++)
-    {
-        sum+=arr[i];
-        if(sum==k)
-        {
-            cout<<0<<" "<<i<<"\n";
-            return;
-        }
-        if(num.find(sum-k)!=num.end())
-        {
-            //cout<<"sum: "<<sum<<"\n";
-            cout<<num[sum-k]<<" "<<i<<"\n";
-            return;
-        }
-
-        num[sum]=i+1;
-    }
-
-    cout<<"No Subarray found\n";
-    return ;
-}
-
 int main()
 {
     io;
-    ll test_case;   //test_case input
+    ll test_case;
     cin>>test_case;
-    //test_case=1;
     while(test_case--)
     {
-        ll n,k;
-        cin>>n>>k;
+        ll n,s;
+        cin>>n>>s;
         ll arr[n];
-        for(int i=0;i<n;i++) cin>>arr[i];
+        for(int i=1;i<=n;i++) cin>>arr[i];
 
-        subarray(arr,n,k);
+        int i=1,j=1,sum=0;
+        while(j<=n)
+        {
+            while(sum>s&&i<=j)
+            sum-=arr[i],i++;
+
+            if(sum==s)
+            break;
+
+            else
+            sum+=arr[j],j++;
+        }
+        while(sum>s&&i<=j)
+            sum-=arr[i],i++;
+
+        if(sum!=s)
+        print(-1);
+        else
+        print2(i,j-1);
     }
-    return 0;
 }
