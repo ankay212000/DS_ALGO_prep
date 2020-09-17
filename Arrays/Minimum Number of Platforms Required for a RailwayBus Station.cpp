fff69c4effe82686e0a63a258fe803d412568431
ANKAY <52236029+ankay212000@users.noreply.github.com>
2020-09-17 13:32:56 +0530

    Create Minimum Number of Platforms Required for a RailwayBus Station.cpp

---
+++ Arrays/Minimum Number of Platforms Required for a RailwayBus Station.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+using namespace std;
+#define N 100000
+#define limit 10000
+ 
+template <typename T>
+void print(T x){cout<<x<<"\n";}
+template <typename T1, typename T2>
+void print2(T1 x,T2 y){cout<<x<<" "<<y<<"\n";}
+template <typename T1, typename T2,typename T3>
+void print3(T1 x, T2 y,T3 z){cout<<x<<" "<<y<<" "<<z<<"\n";}
+
+int main()
+{
+    io;
+    ll test_case;
+    //cin>>test_case;
+    test_case=1;
+    while(test_case--)
+    {
+        int n;
+        cin>>n;
+        vector<int> arrival(n);
+        vector<int> departure(n);
+        int platform[2361]={};
+        int mintime=INT_MAX,maxtime=INT_MIN;
+        for(int i=0;i<n;i++) cin>>arrival[i],mintime=min(mintime,arrival[i]);
+        for(int i=0;i<n;i++) cin>>departure[i],maxtime=max(maxtime,departure[i]);
+        for(int i=0;i<n;i++)
+        {
+            platform[arrival[i]]++;
+            platform[departure[i]]--;
+        }
+
+        int ans=0,sum=0;
+        for(int i=mintime;i<=maxtime;i++)
+        {
+            sum+=platform[i];
+            ans=max(ans,sum);
+        }
+
+        print(ans);
+    }
+}
